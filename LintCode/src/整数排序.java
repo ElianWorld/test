1df342da04e66bea70b23a1c29401a891928327d
World <1370420477@qq.com>
2017-07-19 17:53:14 +0800

    整数排序

---
+++ LintCode/src/整数排序.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ÕûÊıÅÅĞò {
+	/**
+     * @param A an integer array
+     * @return void
+     */
+    public void sortIntegers(int[] A) {
+        // Write your code here
+        Arrays.sort(A);
+    }
+
+}
