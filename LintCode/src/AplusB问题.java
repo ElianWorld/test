300c1d6f851078fbbe4548a3a62884108f6a09a1
World <1370420477@qq.com>
2017-07-19 19:12:09 +0800

    A+B问题

---
+++ LintCode/src/AplusB问题.java
@@ -0,0 +1,21 @@
+
+public class AplusBÎÊÌâ {
+	/*
+     * param a: The first integer
+     * param b: The second integer
+     * return: The sum of a and b
+     */
+    public int aplusb(int a, int b) {
+        // write your code here, try to do it without arithmetic operators.
+        int sum = 0;
+        int flag = 1;
+        while(flag != 0)
+        {
+            sum = a ^ b;
+            flag = (a & b) << 1;
+            a = sum;
+            b = flag;
+        }
+        return sum;
+    }
+}
