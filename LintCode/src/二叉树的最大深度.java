a09d61fede9b11cc62fea61f0a5aa1f447249aa4
World <1370420477@qq.com>
2017-07-19 18:32:17 +0800

    二叉树的最大深度

---
+++ LintCode/src/二叉树的最大深度.java
@@ -0,0 +1,32 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+class TreeNode {
+     public int val;
+     public TreeNode left, right;
+     public TreeNode(int val) {
+         this.val = val;
+         this.left = this.right = null;
+     }
+}
+public class 二叉树的最大深度 {
+	/**
+     * @param root: The root of binary tree.
+     * @return: An integer.
+     */
+    public int maxDepth(TreeNode root) {
+        // write your code here
+        if(root == null) return 0;
+        int right = maxDepth(root.right) + 1;
+        int left = maxDepth(root.left) + 1;
+        return right < left ? left : right;
+    }
+}
